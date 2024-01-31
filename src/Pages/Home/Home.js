3c5ef3411ff3f4f889511d7e11ba9428c7d37a4a
Nanji Lakan <86968487+Shaelle11@users.noreply.github.com>
2024-01-31 04:38:46 +0100

    changed api url

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Post from "../../Components/Post/Post";
-import PostCarousel from "../../Components/PostCarousel/PostCarousel";
+// import PostCarousel from "../../Components/PostCarousel/PostCarousel";
 import "./Home.css"
 
 export default function Home(){
-  const [currentSlide, setCurrentSlide] = useState([]);
+  // const [currentSlide, setCurrentSlide] = useState([]);
   const [posts, setPosts] = useState([]);
   
   const url = 'https://mern-blog-server-psi.vercel.app/post';
 
-  useEffect(() => {
-fetch(url).then(response =>{
-  response.json().then(currentSlide => {
-setCurrentSlide(currentSlide);
-  } )
-})
-  })
+//   useEffect(() => {
+// fetch(url).then(response =>{
+//   response.json().then(currentSlide => {
+// setCurrentSlide(currentSlide);
+//   } )
+// })
+//   })
  
     useEffect(() => {
      fetch(url).then(response => {
@@ -26,9 +26,9 @@ setCurrentSlide(currentSlide);
     })
     return(
         <main className="home">
-          {currentSlide.length> 0 && currentSlide.map(current =>(
+          {/* {currentSlide.length> 0 && currentSlide.map(current =>(
             <PostCarousel {...current}/>
-          ) )}
+          ) )} */}
           {posts.length> 0 && posts.map(post => (
             <Post {...post}/>
           ))}
